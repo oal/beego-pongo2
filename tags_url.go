b3575baf205de2c6b99c9eaa14873234ef98c4fe
Olav Andreas Lindekleiv <olav@lindekleiv.com>
2014-07-06 21:17:10 +0200

    Check number of arguments after parsing.

--- tags_url.go
+++ tags_url.go
@@ -28,17 +28,17 @@ func (node *tagURLNode) Execute(ctx *p2.ExecutionContext) (string, error) {
 // urlfor takes one argument for the controller, as well as any number of key/value pairs for additional URL data.
 // Example: {% url "UserController.View" ":slug" "oal" %}
 func tagURLForParser(doc *p2.Parser, start *p2.Token, arguments *p2.Parser) (p2.INodeTag, error) {
-	if (arguments.Count()-1)%2 != 0 {
-		return nil, arguments.Error("URL takes one argument for the controller and any number of optional pairs of key/value pairs.", nil)
-	}
-
 	evals := []p2.INodeEvaluator{}
 	for arguments.Remaining() > 0 {
-		eval, err := arguments.ParseExpression()
-		evals = append(evals, eval)
+		expr, err := arguments.ParseExpression()
+		evals = append(evals, expr)
 		if err != nil {
 			return nil, err
 		}
+	}
+
+	if (len(evals)-1)%2 != 0 {
+		return nil, arguments.Error("URL takes one argument for the controller and any number of optional pairs of key/value pairs.", nil)
 	}
 
 	return &tagURLNode{evals}, nil
